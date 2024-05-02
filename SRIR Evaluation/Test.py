6af53e9403c561fa5000c177a32f1d2c8989d13e
Benedicta <benedictamarietta.amanda@student.kuleuven.be>
2024-05-02 13:57:31 +0200

    added base files

---
+++ SRIR Evaluation/Test.py
@@ -0,0 +1,77 @@
+import math
+import numpy as np
+import matplotlib.pyplot  as plt
+import pandas as pd
+import csv
+import os
+
+from read_csv import get_SRIR
+'''
+    Normalize the length of the distance vector for a given point in the SRIR
+'''
+def normalize_len(arr):
+    if len(arr) < 4:
+        return arr
+    arr[-3:] = arr[-3:] / np.linalg.norm(arr[-3:])
+    return arr
+
+'''
+    Plot x and y of a set of SRIR points
+'''
+def plot2D(*arrs):
+    # Extract x, y coordinates
+    points = np.array(arrs)
+    x_coords = points[:, 1]
+    y_coords = points[:, 2]
+    # Create the plot
+    plt.figure()
+    # Plot the points
+    plt.scatter(x_coords, y_coords, c='r', marker='o')
+    # Set labels
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    # Show the plot
+    plt.grid(True)
+    plt.show()
+
+'''
+    Calculate average virtual source
+'''
+def cluster_arrs(*arrs):
+    sum_of_p = 0
+
+    sum_of_x = 0
+    sum_of_y = 0
+    sum_of_z = 0
+
+    for arr in arrs:
+        arr = normalize_len(arr)
+        sum_of_p += arr[0]
+        sum_of_x += arr[0]*arr[1]
+        sum_of_y += arr[0]*arr[2]
+        sum_of_z += arr[0]*arr[3]
+
+    #sum_of_x = sum_of_x/sum_of_p
+
+    p_avg = sum_of_p/len(arrs)
+    return [p_avg, sum_of_x, sum_of_y, sum_of_z]
+
+print(len(get_SRIR()))
+    #
+    # # Concatenate DataFrames into one long array
+    # combined_df = pd.concat(srir, ignore_index=True)
+    #
+    # # Convert DataFrame to array
+    # combined_array = combined_df.to_numpy()
+
+
+
+
+
+
+
+
+
+# a1 = [1, 2,4,2]
+# a2 = [2, 2,2,0]
+# a3 = [1, 2,-2,0]
---
+++ SRIR Evaluation/read_csv.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import csv
+import os
+
+def get_SRIR():
+    srir_pos = []
+    srir_neg = []
+    folder_path = 'csvs/'
+
+    # Iterate over pos CSV
+    for filename in os.listdir(folder_path):
+        file1_path = os.path.join(folder_path, "11_13_pos12_p_pos.csv")
+        file2_path = os.path.join(folder_path, "11_13_pos12_pos.csv")
+
+        # Open and read both CSV files in parallel
+        with open(file1_path, 'r') as file1, open(file2_path, 'r') as file2:
+            reader1 = csv.reader(file1)
+            reader2 = csv.reader(file2)
+
+            # Iterate through rows in parallel
+            for row1, row2 in zip(reader1, reader2):
+                # Append data from each row to respective lists
+                srir_pos += [row1 + row2]
+    # Iterate over pos CSV
+    for filename in os.listdir(folder_path):
+        file1_path = os.path.join(folder_path, "11_13_pos12_p_neg.csv")
+        file2_path = os.path.join(folder_path, "11_13_pos12_neg.csv")
+
+        # Open and read both CSV files in parallel
+        with open(file1_path, 'r') as file1, open(file2_path, 'r') as file2:
+            reader1 = csv.reader(file1)
+            reader2 = csv.reader(file2)
+
+            # Iterate through rows in parallel
+            for row1, row2 in zip(reader1, reader2):
+                # Append data from each row to respective lists
+                srir_neg += [row1 + row2]
+
+    # print("pos len: " + str(len(srir_pos)) )
+    # print("neg len: " + str(len(srir_neg)) )
+    return srir_pos + srir_neg
